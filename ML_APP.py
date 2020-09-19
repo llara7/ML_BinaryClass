55e79cbb0d006c564ad3219e0e212eee88c8d8d3
llara7 <37033691+llara7@users.noreply.github.com>
2020-09-19 18:00:16 -0500

    Update ML_APP.py

--- ML_APP.py
+++ ML_APP.py
@@ -27,7 +27,7 @@ def main():
 
     @st.cache(persist=True)
     def load_data():
-        data = pd.read_csv("/Users/lauralara/PycharmProjects/MachineLearning/mushrooms.csv")
+        data = pd.read_csv("mushrooms.csv")
         labelencoder = LabelEncoder()
         for col in data.columns:
             data[col] = labelencoder.fit_transform(data[col])
